49498a9bec8d126692503b18a6e13f7a235ef4d6
ilkerakkaya <ilkerakkaya@gmail.com>
2019-08-05 19:43:43 +0200

    homework7

---
+++ app.js
@@ -0,0 +1,66 @@
+
+
+
+$(document).on('click','.delete', function () {
+    event.preventDefault();
+
+    var muzik2= new Audio( "audio/finish-him.mp3");
+        muzik2.play();
+   
+    $(this).closest('li').remove();
+   
+ });
+
+
+ 
+ $('button').on('click', function (e) {
+    e.preventDefault();
+    let newValue = $('#addListItem').val();
+
+        if (newValue !== ''){
+
+        
+         $('ul').append("<li><span class='name'>"
+         + newValue
+         + "</span><span class='delete' >delete</span>");
+     $('#addListItem').val(''); 
+    
+     var muzik= new Audio( "audio/at.mp3");
+     muzik.play();
+    
+    }
+
+    else {
+        $(this).vibrate(); 
+    }
+
+     
+
+
+     
+ });
+
+
+ 
+
+
+ 
+ $('#hide').click(function () {
+    $('ul').toggle(this.unchecked);
+ });
+ 
+ 
+ 
+
+
+ 
+ //search
+ $('#search-topics input').keyup(function () {
+    var searchText = $(this).val();
+    $('ul > li').each(function () {
+        var currentLiText = $(this).text(),
+            showCurrentLi = currentLiText.indexOf(searchText) !== -1;
+        $(this).toggle(showCurrentLi);
+    });
+ })
+ 
